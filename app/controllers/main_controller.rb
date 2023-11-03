fc96e65d423fe03a085d72b1a509469c325ba95b
Daniela <daniela.correa@alu.ing.unlp.edu.ar>
2023-11-03 18:23:10 -0300

    Flash messages practice

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,7 @@
 class MainController < ApplicationController
     def index
+        flash[:notice] = "Logged in successfully"
+        flash[:alert] = "Invalid email or password"
+        # Se pueden declarar los mensajes flash aca, pero hay que printearlos en algun lugar del html
     end;    
 end    
