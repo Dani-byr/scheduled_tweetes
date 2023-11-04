5b6f896085f88d356a16df0ba14da200b6cc35f0
Daniela <daniela.correa@alu.ing.unlp.edu.ar>
2023-11-04 15:14:46 -0300

    Sign up with session cookies

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,9 @@
 class MainController < ApplicationController
-    def index
-        flash.now[:notice] = "Logged in successfully"
-        flash.now[:alert] = "Invalid email or password"
+    def index   
+        if session[:user_id]
+            @user = User.find(session[:user_id])
+        end
+       
         # Se pueden declarar los mensajes flash aca, pero hay que printearlos en algun lugar del html
     end;    
 end    
--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -7,8 +7,10 @@ class RegistrationController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             redirect_to root_path, notice: "Successfully created account"
         else
+            flash[:alert]
             render :new
         end
     end
